6fec680f29993f983e894f288d107334cd17e450
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-11-19 14:51:28 +0545

    hcm lcf update

--- hcf_lcm.c
+++ hcf_lcm.c
@@ -14,10 +14,19 @@ int main() {
     return 0;
 }
 
+// int hcf(int num1, int num2) {
+//     if (num2 == 0) {
+//         return num1;
+//     } else {
+//         return hcf(num2, num1 % num2);
+//     }
+// }
 int hcf(int num1, int num2) {
-    if (num2 == 0) {
-        return num1;
-    } else {
-        return hcf(num2, num1 % num2);
+    int temp;
+    while (num2 != 0) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp%num2;
     }
+    return num1;
 }
