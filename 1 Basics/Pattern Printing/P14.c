cb32ca5d4e427502e278027ea04e281ff58872de
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-16 10:46:42 +0545

    add new problem

---
+++ 1 Basics/Pattern Printing/P14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (char j = 'A'; j <= 'A' + i; j++)
+        {
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
